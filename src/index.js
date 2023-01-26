6ddca5820f7644aa4e3b774c36ad1a33efde66c7
a-reznikov <a.reznnikov@gmail.com>
2023-01-26 15:45:31 +0300

    feat: implement solushion

--- src/index.js
+++ src/index.js
@@ -14,12 +14,14 @@ module.exports = function check(str, bracketsConfig) {
     let noDouble = '';
     let bracketNumber = 0;
     let count = 0;
+    let count7 = 0;
+    let count8 = 0;
     for (let brackets = 0; brackets < bracketsArray.length - 1;) {
       brackets ++;
       //console.log('_____________________________________');
       //console.log('bracketsArray in Enter', bracketsArray);
       //console.log('number', brackets);
-      if (bracketsArray[brackets] === ')' || bracketsArray[brackets] === ']'|| bracketsArray[brackets] === '}') {
+      if (bracketsArray[brackets] === ')' || bracketsArray[brackets] === ']'|| bracketsArray[brackets] === '}'|| bracketsArray[brackets] === '2'|| bracketsArray[brackets] === '4'|| bracketsArray[brackets] === '6') {
         //console.log('brackets',bracketsArray[brackets], 'find on number', brackets);
         bracketNumber = brackets;
         bracketDelete (bracketsArray[brackets]);
@@ -48,7 +50,41 @@ module.exports = function check(str, bracketsConfig) {
             brackets = 0;
             count = 0;
           }
-      }
+      } else if (bracketsArray[brackets] === '7') {
+        //console.log('First brackets', bracketsArray[brackets], 'find', brackets);
+        count7++;
+        //console.log(count);
+          if (count7 === 2) {
+            //console.log('Second brackets', bracketsArray[brackets], 'find', brackets);
+            bracketNumber = brackets;
+            bracketDelete (bracketsArray[brackets]);
+            brackets = 0;
+            count7 = 0;
+          } else if (bracketsArray[0] === '7' && bracketsArray[1] === '7') {
+            //console.log('Towords first and second brackets', bracketsArray[brackets], 'find', brackets);
+            bracketNumber = brackets;
+            bracketDelete (bracketsArray[brackets]);
+            brackets = 0;
+            count7 = 0;
+          }
+      } else if (bracketsArray[brackets] === '8') {
+        //console.log('First brackets', bracketsArray[brackets], 'find', brackets);
+        count8++;
+        //console.log(count);
+          if (count8 === 2) {
+            //console.log('Second brackets', bracketsArray[brackets], 'find', brackets);
+            bracketNumber = brackets;
+            bracketDelete (bracketsArray[brackets]);
+            brackets = 0;
+            count8 = 0;
+          } else if (bracketsArray[0] === '8' && bracketsArray[1] === '8') {
+            //console.log('Towords first and second brackets', bracketsArray[brackets], 'find', brackets);
+            bracketNumber = brackets;
+            bracketDelete (bracketsArray[brackets]);
+            brackets = 0;
+            count8 = 0;
+          }
+      } 
 
       
     }
@@ -62,7 +98,17 @@ module.exports = function check(str, bracketsConfig) {
         oposition = '{';
       } else if (bracket === '|') {
         oposition = '|';
-      }
+      } else if (bracket === '2') {
+        oposition = '1';
+      } else if (bracket === '4') {
+        oposition = '3';
+      } else if (bracket === '6') {
+        oposition = '5';
+      } else if (bracket === '7') {
+        oposition = '7';
+      } else if (bracket === '8') {
+        oposition = '8';
+      } 
       let sliceArray = bracketsArray.slice(0, bracketNumber + 1);
       let endSlice = bracketNumber;
       //console.log('sliceArray', sliceArray);
@@ -94,10 +140,10 @@ module.exports = function check(str, bracketsConfig) {
     }
     let residueBracketsArray = bracketsArray.join('');
     if (bracketsArray.length > 0) {
-      noDouble += residueBracketsArray;
+      residueBracketsArray += noDouble;
     }
     //console.log('noDouble', noDouble);
-    compare.push(noDouble);
+    compare.push(residueBracketsArray);
     console.log('compare', compare);
   }
   return compare[0] == compare[1] ? true : false;
